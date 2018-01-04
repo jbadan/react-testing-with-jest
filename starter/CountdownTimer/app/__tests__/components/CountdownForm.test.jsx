f8ff1fc0f252d149c24f6036f50453ffb8b62e34
jbadan <jenna.badanowski@gmail.com>
2018-01-03 16:11:26 -0800

    further testing

---
+++ starter/CountdownTimer/app/__tests__/components/CountdownForm.test.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+import CountdownForm from '../../components/CountdownForm';
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('CountdownForm', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<CountdownForm/>, div);
+    });
+
+    it('should call onSetCountdownTime if valid seconds entered', () => {
+        const spy = jest.fn();
+        const countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdownTime={spy}/>);
+        const form = TestUtils.findRenderedDOMComponentWithTag(countdownForm, 'form');
+
+        countdownForm.refs.seconds.value = '109';
+        TestUtils.Simulate.submit(form);
+
+        expect(spy).toHaveBeenCalledWith(109);
+    });
+
+    it('should not call onSetCountdownTime if invalid seconds entered', () => {
+        const spy = jest.fn();
+        const countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdownTime={spy}/>);
+        const form = TestUtils.findRenderedDOMComponentWithTag(countdownForm, 'form');
+
+        countdownForm.refs.seconds.value = '1H63';
+        TestUtils.Simulate.submit(form);
+
+        expect(spy).not.toHaveBeenCalled();
+    });
+});
